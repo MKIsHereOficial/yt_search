316e4ebe31a982b22ec745d234c0265a9c6abc5b
Minna <mkishere.oficial@gmail.com>
2020-10-22 19:45:44 -0300

    Atualização de versão e adição da multi-search, utilizando o yts.search, retorna um array de vídeos utilizando um número como segundo parâmetro para limitá-lo. Uma atualização será efetuada na README.md

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const version = require("./package.json").version;
+
 const yts = require( 'yt-search' );
 
 let language = "pt";
@@ -11,7 +13,7 @@ async function traduzir (value) {
 }
 
 async function search (value) {
-	if (!value) throw new Error("O Valor da Pesquisa não foi identificado. Analise se é uma String.");
+	if (!value) throw new Error("O Valor da Pesquisa não foi identificado. VVerifique se é uma String.");
 
 	const r = await yts(value);
 
@@ -67,7 +69,73 @@ async function search (value) {
 
   return obj;
 }
+async function multi_search(value, maxVideos) {
+	if (!value) throw new Error("O Valor da Pesquisa não foi identificado. Verifique se é uma String.");
 
+	if (!maxVideos) {
+		console.log(`Você não espeficou o parâmetro maxVideos. Este deve ser um número, no qual especifica-se o número máximo de vídeos no array.`)
+		maxVideos = 5;
+	} else if (isNaN(maxVideos)) {
+		throw new Error("O parâmetro maxVideos deve ser um número, no qual especifica-se o número máximo de vídeos no array.");
+	}
+
+	const results = await yts(value);
+
+	const vids = results.videos.slice( 0, maxVideos );
+	const videos = videos.map(video => {
+		let obj = {
+			duration: video.timestamp,
+			title: video.title,
+			url: video.url,
+			id: video.videoId,
+			description: video.description,
+			imageURL: function get () {
+				return video.image;
+			},
+			thumbnailURL: function get () {
+				return video.thumbnail;
+			},
+			ago: (async (event) => {
+				let a = video.ago;
+				let b = await traduzir("Indisponível");
+
+				if (!a) {
+					return b;
+				}
+
+				await translate(a, {to: language}).then(res => {
+					return b = res.text;
+				}).catch(err => {
+	    		throw console.error(err);
+				});
+
+	      if (language != "en") {
+				     return b;
+	      }
+
+	      return a;
+			})(),
+			author: {
+				name: video.author.name,
+				url: video.author.url,
+				},
+
+			views: video.views.toLocaleString(),
+			downloadURL: function get () {
+				let url = `https://www.y2mate.com/pt/youtube/${this.id}`;
+
+				return url;
+			}
+
+		};
+
+	  return obj;
+	});
+
+	return videos;
+}
 
 module.exports = search;
 module.exports.language = language;
+module.exports.version = version;
+module.exports.search = multi_search;
