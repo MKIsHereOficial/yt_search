a17090170cf8b548fe3ebaa71fbda87dcd48bd86
Minna <mkishere.oficial@gmail.com>
2020-11-06 14:51:33 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 async function test() {
   const yts = await require('./main.js');
-  let video = await yts('Coffe for your head');
+  let video = await yts('beabadoobee - Coffe for your head remix');
 
   console.log(video);
 }
